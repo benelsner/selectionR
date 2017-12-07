11bffb6f75ec412ceea79dbf5b8fe1e6ed0d538a
benelsner <elsnerb@tcd.ie>
2017-12-07 07:24:11 +0100

    deleted old algorithm

--- fxfind.R
+++ fxfind.R
@@ -5,36 +5,6 @@
 #####################################################
 
 
-#vec_fx1<-as.matrix(seq(from=1, to=grid_fx, by=1)) # set up grid
-#vec_fx2<-as.matrix(seq(from=0.1, to=0.9, by=0.1)) #  fixed costs below 1 (a possibility they are in one country)
-#vec_fx<-rbind(vec_fx2, vec_fx1)
-vec_fx<-seq(from=0.2, to=2, by=0.2)
-
-gridfx<-as.matrix(expand.grid(x=vec_fx, y=vec_fx))
-gridcells_fx<-dim(gridfx)[1]
-
-vars.list<-list(vars)[rep(1,dim(gridfx)[1])] # make list with parameters; fx differ in each cell of list
-for (i in 1:gridcells_fx){
-  vars.list[[i]][[8]]<-gridfx[i,]
-}
-
-z_bar_fx<-t(sapply(1:gridcells_fx, function(i) findeq(skills[[1]], vars.list[[i]]))) # find the zbar in each fx combination
-z_bar_fx<-split(z_bar_fx, seq(nrow(z_bar_fx)))
-
-
-shwork_y_fx<-sapply(1:gridcells_fx, function(i) outputs(z_bar_fx[[i]], skills[[1]], vars.list[[i]])[[2]]) # compute share of workers in each fx combination
-
-# find values with minimum squared distance
-  mse_y<-function(shwork_y_fx){
-    mse<-(target_shy_us-shwork_y_fx[1])^2+(target_shy_mex-shwork_y_fx[2])^2 
-    return(mse)
-  }
-  mse_shy<-matrix(unlist(lapply(shwork_y_fx, mse_y)), nrow=length(shwork_y_fx), byrow=TRUE)
-  indmin<-as.matrix(which(mse_shy == min(mse_shy), arr.ind = TRUE))[1,] # ATTENTION: check if there are duplicates
-  fxmin<-gridfx[indmin[1],] 
-  
-  shwork_y_opt<-shwork_y_fx[[indmin[1]]]
-  
   
 #############################
   # stepwise algorithm
